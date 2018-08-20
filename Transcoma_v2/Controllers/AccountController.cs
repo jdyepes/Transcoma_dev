cf9fdf164496e28a3236c01c8870c2c1b6ca585a
jdyepes <jesusyepes.1205@gmail.com>
2018-08-20 02:25:59 -0400

    Preparando ambiente

---
+++ Transcoma_v2/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Transcoma_v2.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult RegistrarUsuario()
+        {
+            return View();
+        }
+
+        public ActionResult IniciarSesion()
+        {
+            return View();
+        }
+    }
+}
---
+++ Transcoma_v2/Models/Account/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Transcoma.Models.Account
+{
+    public class Usuario
+    {
+        public string _primerNombre { get; set; }
+        public string _segundoNombre { get; set; }
+        public string _primerApellido { get; set; }
+        public string _segundoApellido { get; set; }
+        public string _rolUsuario { get; set; }  //administrador - cliente
+        public string _nombreUsuario { get; set; }
+        public string _correo { get; set; }
+        public string _password { get; set; }
+        public string _confirmarPassword { get; set; }
+
+    }
+}
