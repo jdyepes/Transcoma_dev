4e5db5c9bda8b4feef68a4b3e7178def27742fae
jdyepes <jesusyepes.1205@gmail.com>
2018-08-25 14:56:48 -0400

    Modificando estilo de margen, colores - Colocando datos de prueba

--- Transcoma_v2/Models/Account/Usuario.cs
+++ Transcoma_v2/Models/Account/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +8,14 @@ namespace Transcoma.Models.Account
 {
     public class Usuario
     {
-        public string _primerNombre { get; set; }
-        public string _segundoNombre { get; set; }
-        public string _primerApellido { get; set; }
-        public string _segundoApellido { get; set; }
-        public string _rolUsuario { get; set; }  //administrador - cliente
+        //public string _primerNombre { get; set; }
+        //public string _segundoNombre { get; set; }
+        //public string _primerApellido { get; set; }
+        //public string _segundoApellido { get; set; }
+      //  [Required(ErrorMessage = "El nombre es requerido")]
         public string _nombreUsuario { get; set; }
+        public string _rolUsuario { get; set; }  //administrador - cliente
+        public string _organizacion { get; set; } //organizacion/empresa 
         public string _correo { get; set; }
         public string _password { get; set; }
         public string _confirmarPassword { get; set; }
