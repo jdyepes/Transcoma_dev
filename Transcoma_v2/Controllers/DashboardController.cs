f6e8463dffe6f0147d366ffaf47cd07b865f37c5
jdyepes <jesusyepes.1205@gmail.com>
2018-08-27 00:24:48 -0400

    Creando estructura del panel principal-menu. Se crea la controladora no Rest y su vista

---
+++ Transcoma_v2/Controllers/DashboardController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Transcoma.Models.Account;
+
+namespace Transcoma_v2.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Menu()
+        {
+            Usuario obj = new Usuario();
+            //  obj._rolUsuario = "Administrador";
+            obj._nombreUsuario = Request.Form["usuario"].ToString();
+            obj._rolUsuario = Request.Form["roles"].ToString();
+            obj._organizacion = Request.Form["organizacion"].ToString();
+            obj._correo = Request.Form["correo"].ToString();
+            obj._password = Request.Form["password"].ToString();
+            return View(obj);
+            //return View();
+        }
+    }
+}
