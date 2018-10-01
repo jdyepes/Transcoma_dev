54831f52075dc0171416188ceea9d9db59c3f78a
jdyepes <jesusyepes.1205@gmail.com>
2018-09-30 20:22:33 -0400

    Creando la clase Comando para la logica de negocio

---
+++ Backend/TranscomaAPI/Logica de Negocio/Implementacion/Comando/Comando.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TranscomaAPI.Logica_de_Negocio.Implementacion.Comando
+{
+    public abstract class Comando
+    {
+    }
+}
