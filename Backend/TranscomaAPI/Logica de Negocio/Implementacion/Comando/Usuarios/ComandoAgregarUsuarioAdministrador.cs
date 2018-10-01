553b417cfcf2b24c07a8b30557fe51764d4a0bd5
jdyepes <jesusyepes.1205@gmail.com>
2018-09-30 20:23:48 -0400

    Creando los comando asociados al usuario dentro de la logica de negocio

---
+++ Backend/TranscomaAPI/Logica de Negocio/Implementacion/Comando/Usuarios/ComandoAgregarUsuarioAdministrador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TranscomaAPI.Logica_de_Negocio.Implementacion.Comando.Usuarios
+{
+    public class ComandoAgregarUsuarioAdministrador : Comando
+    {
+        public ComandoAgregarUsuarioAdministrador()
+        {
+        }
+    }
+}
---
+++ Backend/TranscomaAPI/Logica de Negocio/Implementacion/Comando/Usuarios/ComandoConsultarUsuarioCliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TranscomaAPI.Logica_de_Negocio.Implementacion.Comando.Usuarios
+{
+    public class ComandoConsultarUsuarioCliente : Comando
+    {
+        public ComandoConsultarUsuarioCliente()
+        {
+        }
+    }
+}
