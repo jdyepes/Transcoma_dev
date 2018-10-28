b3066c0528e1a2b6ec7fcd21a5a31edcf42f17fd
jdyepes <jesusyepes.1205@gmail.com>
2018-10-28 01:41:44 -0400

    Agregando modal en el modulo de tracking

---
+++ frontend/src/app/components/tracking-modal/tracking-modal.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Inject} from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { EntradaElements } from 'src/app/core/tracking/tracking.component';
+
+@Component({
+  selector: 'app-tracking-modal',
+  templateUrl: './tracking-modal.component.html',
+  styleUrls: ['./tracking-modal.component.css']
+})
+export class TrackingModalComponent {
+  constructor(
+    public dialogRef: MatDialogRef<TrackingModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public entrada: EntradaElements ) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
